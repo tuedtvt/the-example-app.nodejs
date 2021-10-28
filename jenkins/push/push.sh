2a07e166eac32a7d1f7b61ca10f430b655d6e7f2
TueNT <TueNT@Tues-MacBook-Pro.local>
2021-10-28 17:53:07 +0700

    v4

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -4,14 +4,14 @@ echo "********************"
 echo "** Pushing image ***"
 echo "********************"
 
-IMAGE="the_example_nodejs_app"
+IMAGE="the_example_nodejs_app:latest"
 
-BUILD_TAG = "latest"
+
 echo "** Logging in ***"
 docker login -u tuedtvt -p $PASS
 echo "*** Tagging image ***"
-echo $IMAGE:$BUILD_TAG
+echo $IMAGE
 
-docker tag $IMAGE:$BUILD_TAG tuedtvt/$IMAGE:$BUILD_TAG
+docker tag $IMAGE tuedtvt/$IMAGE
 echo "*** Pushing image ***"
-docker push tuedtvt/$IMAGE:$BUILD_TAG
+docker push tuedtvt/$IMAGE
