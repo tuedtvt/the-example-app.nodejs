913d60ee34fbb4b1f08a704dfffc63abd27fbd08
TueNT <TueNT@Tues-MacBook-Pro.local>
2021-10-28 13:28:49 +0700

    change jenkins

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+WORKSPACE=/var/jenkins_home/workspace/the_nodejs_examble_pipeline
 
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
-
+root
 cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
